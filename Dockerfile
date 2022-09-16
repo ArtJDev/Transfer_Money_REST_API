582973af76f96081b84475874e7c5ec78ab0ad89
ArtJDev <artemkurilenko1983@yandex.ru>
2022-09-17 00:46:47 +0300

    Commit #1 17.09.2022

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM adoptopenjdk/openjdk11:alpine-jre
 EXPOSE 5500
-ADD target/Conditional-0.0.1-SNAPSHOT.jar prod.jar
+ADD target/Transfer_Money_REST_API-0.0.1-SNAPSHOT.jar TransferMoneyRestAPI.jar
+#CMD ["java", "java", "-jar", "TransferMoneyRestAPI.jar"]
 ENTRYPOINT ["java", "-jar", "TransferMoneyRestAPI.jar"]
--- src/test/java/ru/netology/TransferServiceTestcontainersTest.java
+++ src/test/java/ru/netology/TransferServiceTestcontainersTest.java
@@ -1,43 +1,42 @@
 package ru.netology;
 
+import com.zaxxer.hikari.hibernate.HikariConnectionProvider;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.testcontainers.containers.GenericContainer;
+import ru.netology.dto.Amount;
+import ru.netology.dto.TransferRequest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TransferServiceTestcontainersTest {
-//    @Autowired
-//    private TestRestTemplate restTemplate;
-//
-//    private static final GenericContainer<?> appRestApi = new GenericContainer<>("TransferMoneyRestAPI")
-//            .withExposedPorts(5500);
-//
-//    @BeforeAll
-//    public static void setUp() {
-//        appRestApi.start();
-//    }
-//
-//    @Test
-//    void testRequest() {
-//        HttpHeaders headers = this.restTemplate.getForEntity("/transfer", String.class).getHeaders();
-//        assertThat(headers.getLocation()).hasHost("localhost");
-//    }
-//
-//    @Test
-//    void testResponse() {
-//        ResponseEntity<String> response = restTemplate.
-//                getForEntity("http://localhost:" + appRestApi.getMappedPort(5500) + "/transfer", String.class);
-//        assertEquals(response.getStatusCode(), HttpStatus.OK);
-//    }
+    @Autowired
+    private TestRestTemplate restTemplate;
 
+
+    private static final GenericContainer<?> appRestApi = new GenericContainer<>("restapp")
+            .withExposedPorts(5500);
+
+    @BeforeAll
+    public static void setUp() {
+        appRestApi.start();
+    }
+
+    @Test
+    void requestResponseTest() {
+        HttpEntity<TransferRequest> request = new HttpEntity<>(new TransferRequest(
+                1111111111111111L, "11/22", "111",
+                2222222222222222L, new Amount(100, "RUB")));
+        String response = restTemplate.postForObject("http://localhost:" + appRestApi.getMappedPort(5500) + "/transfer",
+                request, String.class);
+        String expectedMessage = "operationId";
+        assertNotNull(response);
+        assertTrue(response.contains(expectedMessage));
+    }
 }
-
